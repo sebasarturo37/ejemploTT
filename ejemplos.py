dc96bbef9f63e1a7dbffc7cb62b40b464054f9a0
sebasarturo37 <sebasarturo37@hotmail.com>
2019-08-28 10:19:59 -0500

    v1.1

--- ejemplos.py
+++ ejemplos.py
@@ -1,1 +1,55 @@
-print ("¡Hello World!")
+#variables
+# int, str, bool, float
+
+
+
+#def persona(nombre, apellido, edad, estudia):
+
+#    print (f"hola", nombre, f"su apellido es: ", apellido, f"Su edad es: ", edad, f"Usted estudia en: ", estudia)
+
+#nombre = input("Digite su nombre: ")
+#apellido = input("Digite su apellido: ")
+#edad = input("Digite edad: ")
+#estudia = input("Donde estudia?: ")
+
+#persona(nombre, apellido, edad, estudia)
+
+def suma (a, b):
+
+    res = a + b
+
+    print("El resultado de la suma es: ", res)
+
+a = int(input("Digite numero A:"))
+b = int(input("Digite numero B: "))
+
+suma(a,b)
+
+def resta (a, b):
+
+    resu = a - b
+
+    print("El resultado de la resta es: ", resu)
+
+resta(a, b)
+
+def div (a, b):
+
+    resul = a / b
+
+    print("El resultado de la division es: ", resul)
+
+div(a, b)
+
+def multi (a, b):
+
+    result = a * b
+
+    print("El resultado de la multiplicacion es: ", result)
+
+multi(a, b)
+
+
+
+
+
